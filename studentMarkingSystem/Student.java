ee3258e950ba01d0f3adec6b0c489a09c6e60e23
Urmila <Urmila.10@student.scu.edu.au>
2024-07-13 11:57:25 -0700

    Array Declare
    Declare the array and implement input for the assignment name

--- studentMarkingSystem/Student.java
+++ studentMarkingSystem/Student.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Write a description of class Student here.
  *
- * @author (your name)
+ * @author Urmila 
  * @version (a version number or a date)
  */
 public class Student
 {
    public static void main(String[] args) {
-       
+        Scanner scanner = new Scanner(System.in);
+        // marks array declaration to store marks
+        ArrayList<Integer> marksArray = new ArrayList<>();
+        
+        // Allow the user to enter the assignment name
+        System.out.print("Please enter the assignment name: ");
+        String nameOfAssignment = scanner.nextLine();
     }
 }
